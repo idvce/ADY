dd2f0f7ea3802967d3702bf7613bfd6eb978137d
dt <datletoan1812@gmail.com>
2026-02-03 11:01:40 +0700

    Update ADY_2.py

--- ADY_2.py
+++ ADY_2.py
@@ -1,6 +1,4 @@
 
-# pip install selenium webdriver-manager
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
